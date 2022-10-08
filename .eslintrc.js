74431e394c3063e9a8e5c77abffa44a0258acf6a
reubber de sousa <reubbers@gmail.com>
2022-10-08 20:40:21 -0300

    chore: eslint/prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,18 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
+        'react/jsx-uses-react': 1,
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
